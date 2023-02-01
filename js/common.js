ec192b6ff60d668456054da98408b4b5f5cc85fe
dlwnsdnsla <122263039+dlwnsdnsla@users.noreply.github.com>
2023-02-01 16:25:56 +0900

    02/01 fixed

--- js/common.js
+++ js/common.js
@@ -1,17 +1,33 @@
 $(function(){
 
 
+      //헤더 scroll
+    $(window).scroll(function(){  
+        var num = $(this).scrollTop();  
+        if( num > 10 ){  
+             $('#header').addClass('on')
+        }
+        else{
+            $("#header").removeClass('on')
+        }
+
+   });
+
+
+
+
+
+
+
+
+
+
+
 
 
 $(".bar").on("click",function(){
     $(".close").css("display","block");
     });
-
-
-
-
-
-
 
 
 
@@ -24,26 +40,34 @@ $(".close").on("click",function(){
  $(".black_back").css("display","none")
  $(".close").css("display","none")
 });    
+
 $(".m_depth1>li").on("click",function(){
     $(this).find(".m_depth2").stop().slideToggle();
    }); 
 
 
-
-$(".depth1").on("mouseover focus",function(){
-    $(".box_bg").css("display","block");
-   });
-   $(".depth1").on("mouseleave blur",function(){
-    $(".box_bg").css("display","none");
-   });  
-
+// $(".depth1").on("mouseover focus",function(){
+//     $(".box_bg").css("display","block");
+//    });
+//    $(".depth1").on("mouseleave blur",function(){
+//     $(".box_bg").css("display","none");
+//    });  
 
 
 
 
 
 
-
+$('.depth1').on('mouseover focus', function(){
+    $(".box_bg").stop().animate({height:"200px"},100);
+    $(".depth2").stop().animate({top:"0px"},100);
+    $("#header").css("border-bottom","1px solid rgb(189, 189, 189, 0.3)");
+})
+$('.depth1').on('mouseleave blur', function(){
+    $(".box_bg").stop().animate({height:"0"},100);
+    $(".depth2").stop().animate({top:"-50px"},100);
+    $("#header").css("border-bottom","0px solid rgb(189, 189, 189, 0.3)");
+})
 
 
 
