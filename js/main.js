0391eeb73f6f6cee2d4ccb9422d5e524a47ec286
dlwnsdnsla <122263039+dlwnsdnsla@users.noreply.github.com>
2023-02-03 13:39:08 +0900

    slider fix

--- js/main.js
+++ js/main.js
@@ -1,36 +1,16 @@
 jQuery(document).ready(function ($) {
 
-    var jssor_1_options = {
-      $AutoPlay: true,
-      $SlideDuration: 800,
-      $SlideEasing: $Jease$.$OutQuint,
-      $ArrowNavigatorOptions: {
-        $Class: $JssorArrowNavigator$
-      },
-      $BulletNavigatorOptions: {
-        $Class: $JssorBulletNavigator$
-      }
-    };
+    $( '#section .cont1 .slider' ).bxSlider( {
+        auto: true,
+          pause: 3000,
+          autoHover: true,
+          minSlides: 1,
+          maxSlides: 1,
+          slideWidth: 1920,
+          slideMargin: 3,
+          pager:true,
+        } );
 
-    var jssor_1_slider = new $JssorSlider$("jssor_2", jssor_1_options);
-
-    /*responsive code begin*/
-    /*you can remove responsive code if you don't want the slider scales while window resizing*/
-    function ScaleSlider() {
-        var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
-        if (refSize) {
-            refSize = Math.min(refSize, 1920);
-            jssor_1_slider.$ScaleWidth(refSize);
-        }
-        else {
-            window.setTimeout(ScaleSlider, 30);
-        }
-    }
-    ScaleSlider();
-    $(window).bind("load", ScaleSlider);
-    $(window).bind("resize", ScaleSlider);
-    $(window).bind("orientationchange", ScaleSlider);
-    /*responsive code end*/
 
 
 
@@ -60,7 +40,6 @@ jQuery(document).ready(function ($) {
     $('html, body').animate({scrollTop : 0},600);
     return false;
     });
-
 
 
 
